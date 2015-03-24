5ccf8c63327acb198827f43c31423292c05e7892
kailevy <kai0levy@gmail.com>
2015-03-24 13:04:57 -0400

    Machine Learning toolbox

--- toolbox/ml/learning_curve.py
+++ toolbox/ml/learning_curve.py
@@ -2,6 +2,7 @@
 
 import matplotlib.pyplot as plt
 import numpy
+import random
 from sklearn.datasets import *
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -18,6 +19,12 @@ test_accuracies = numpy.zeros(len(train_percentages))
 # for consistency with the previous example use model = LogisticRegression(C=10**-10) for your learner
 
 # TODO: your code here
+for i in range(len(train_percentages)):
+    for j in range(num_trials):
+        X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size=train_percentages[i])
+        model = LogisticRegression(C=10**-10)
+        model.fit(X_train, y_train)
+        test_accuracies[i] += model.score(X_test,y_test)
 
 fig = plt.figure()
 plt.plot(train_percentages, test_accuracies)
